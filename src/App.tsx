45899598bf35e42dc2f4105ede0a2919a26cd4fa
Jakov <j.jakovu@gmail.com>
2025-09-18 22:39:00 +0300

    chore: update config for deployment and dependencies
    Add vercel.json with rewrite rules for SPA routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Layout } from './ui/components/Layout';
 import { publicRoutes } from './application/router';
 import { Suspense } from 'react';
 import { Provider } from 'react-redux';
 import { store } from './application/store';
 import Background from './ui/components/Background';
-import { Layout } from './ui/components/Layout';
 
 function App() {
   return (
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter>
         <Suspense>
           <Background />
           <Routes>
-            {/* Родительский маршрут с Layout */}
             <Route element={<Layout />}>
               {publicRoutes.map((route) => (
                 <Route key={route.path} path={route.path} element={<route.component />} />
@@ -21,7 +20,7 @@ function App() {
             </Route>
           </Routes>
         </Suspense>
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   );
 }
